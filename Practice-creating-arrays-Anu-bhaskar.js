d18c557013554e1a2d71697a440b5dbff3ea44d2
Anu <anubhaskar25@gmail.com>
2026-03-11 23:44:13 -0500

    Create Practice-creating-arrays-Anu-bhaskar.js
    task 1 and task2 done

---
+++ Practice-creating-arrays-Anu-bhaskar.js
@@ -0,0 +1,16 @@
+//Task 1: Create Arrays
+let groceryItems = ["Milk", "Bread", "Eggs", "Apples", "Rice"];
+let prices = [3.99, 2.49, 5.99];
+//Task 2: Access and Log the Values
+// Log the entire arrays
+console.log(groceryItems);
+console.log(prices);
+
+// Log three individual grocery items
+console.log(groceryItems[0]); // Milk
+console.log(groceryItems[2]); // Eggs
+console.log(groceryItems[4]); // Rice
+
+// Log two prices
+console.log(prices[0]); // 3.99
+console.log(prices[2]); // 5.99
